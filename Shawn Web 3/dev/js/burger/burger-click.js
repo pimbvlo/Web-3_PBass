301176a9928492523ec45b97ae7a794e2a98e9fe
pimbvlo <38928988+pimbvlo@users.noreply.github.com>
2020-02-03 22:48:14 -0500

    Init Push
    Problems:
    • certain sections are now not showing up

--- Shawn Web 3/dev/js/burger/burger-click.js
+++ Shawn Web 3/dev/js/burger/burger-click.js
@@ -5,17 +5,25 @@ lineToX.to("#burger-container",{duration:burgerSpeed, rotate:-90},"create-x")
 .to("#bottom-line",{duration:burgerSpeed, rotate:-45},"create-x");
 // .from("#burger-outline",{duration:0.5, alpha:0},"create-x");
 
+var staggerNav = gsap.timeline({paused:true});
+staggerNav.to("#nav-bg", {duration: burgerSpeed+(burgerSpeed/2), scale: 50, alpha: 0.9})
+.to("li", {duration:burgerSpeed/4, stagger:0.15, alpha:1, y: 10}, "-=0.15");
+
+
 $("#burger-container").on("click", function(){
     console.log("click");
     if(canYouSeeNav === false){
         gsap.set("#burger-outline",{duration:burgerSpeed, alpha:1});
-        // lineToX.invalidate().restart();
+        lineToX.invalidate().restart();
+        staggerNav.invalidate().restart();
 
         lineToX.play();
+        staggerNav.play();
         canYouSeeNav = true;
     }else{
         gsap.to("#burger-outline",{duration:burgerSpeed, alpha:0});
 
+        staggerNav.reverse();
         lineToX.reverse();
         canYouSeeNav = false;
     }
--- Shawn Web 3/dev/js/burger/burger.js
+++ Shawn Web 3/dev/js/burger/burger.js
@@ -2,6 +2,9 @@ var burgerSpeed = 0.25
 
 
 var canYouSeeNav = false;
+gsap.set("#nav-bg", {transformOrigin:"50% 50%"});
+// gsap.set("#nav-bg svg path", {fill: "#FF5D00"});
+gsap.set("li", {alpha: 0});
 
 // hide outline
 gsap.set("#burger-outline",{alpha:0, transformOrigin:"50% 50%"});
